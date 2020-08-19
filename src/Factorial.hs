1ddbc95b8c6b6b375a0de700af022588d83a5ea3
Fart Ball <fartball@fartball.com>
2020-08-19 13:18:22 +1000

    Minor alterations.

--- src/Factorial.hs
+++ src/Factorial.hs
@@ -29,6 +29,7 @@ Factorial
 )
 where
 
+import Data.Maybe
 
 
 {--
@@ -47,16 +48,60 @@ where
 
 -- Function declaration
 
-factorial :: Integer -> Integer
+factorial :: Integer -> Maybe Integer
 
 -- Function implementation 1
+--
+-- Invoke this implementation from within ghci as;
+--
+--   > factorial (-1)
 
-factorial -1  = 0
+factorial (-1) = Just 0
 
 -- Function implementation 2
 
-factorial 0   = 1
+factorial 0    = Just 1
 
 -- Function implementation 3
+--
+-- 0 is the default value for fromMaybe in case the second arg is Nothing.
 
-factorial x   = x * factorial (x - 1)
+factorial x    = Just (x * fromMaybe 0 (factorial (x - 1)))
+
+
+factorial_new :: Integer -> Maybe Integer
+factorial_new    x
+
+    | (x <  0)  = Nothing
+
+    | (x == 0)  = Just 1
+
+    | otherwise = Just (x * x_next)
+ 
+        where
+
+            x_next_maybe = factorial_new (x - 1)
+
+            -- Get the value from the Maybe x_next_maybe.
+            --
+            -- 0 is the default value for the fromMaybe function. It will be used in the case where
+            -- the function's second argument is equal to Nothing.
+
+            x_next = fromMaybe 0 x_next_maybe
+
+
+displayFactorial :: Integer -> IO ()
+displayFactorial 0 = 
+
+    putStrLn("You chose zero!!!")
+
+displayFactorial 1 = do
+
+    putStrLn("Number = " ++ "1")
+    putStrLn("You chose one!!!")
+
+displayFactorial x = do
+
+    putStrLn("Number = " ++ show x)
+    putStrLn("You DIDN't chose zero or one!!!")
+
